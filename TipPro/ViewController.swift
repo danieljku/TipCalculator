39e416480a5cd688423b70ee654918611093e7a0
danieljku <DKu@Daniels-MacBook-Air.local>
2016-06-24 10:15:26 -0700

    Complete Tip Calculator

--- TipPro/ViewController.swift
+++ TipPro/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billAmountField: UITextField!
+    @IBOutlet weak var tipSelector: UISegmentedControl!
+    @IBOutlet weak var tipAmountField: UITextField!
+    @IBOutlet weak var totalAmountField: UITextField!
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +30,43 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func calculateTip(sender: AnyObject) {
+        guard let billAmount = Double(billAmountField.text!) else{
+            print("You did not enter a bill amount!")
+            
+            billAmountField.text = ""
+            tipAmountField.text = ""
+            totalAmountField.text = ""
+            
+            return
+        }
+        
+        var tipPercent = 0.0
+        
+        switch tipSelector.selectedSegmentIndex{
+        case 0:
+            tipPercent = 0.15
+        case 1:
+            tipPercent = 0.18
+        case 2:
+            tipPercent = 0.20
+        default:
+            break
+        }
+        
+        let roundedBillAmount = round(billAmount * 100) / 100
+        let tipAmount = roundedBillAmount * tipPercent
+        let roundedTipAmount = round(tipAmount * 100) / 100
+        let totalAmount = roundedBillAmount + roundedTipAmount
+        
+        if(!billAmountField.editing){
+            billAmountField.text = String(format: "%.2f", roundedBillAmount)
+        }
+        
+        tipAmountField.text = String(format: "%.2f", roundedTipAmount)
+        totalAmountField.text = String(format: "%.2f", totalAmount)
+        
+    }
+    
 }
 
